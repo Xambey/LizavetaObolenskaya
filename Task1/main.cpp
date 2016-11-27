9e0227a197ea28a5cd9360c9ad12207341dc3479
Xambey <Xambey@yandex.ru>
2016-11-27 18:18:37 +0300

    added comments in task 1

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -7,14 +7,18 @@ int main(int argc, char**argv){
     QWidget wgt;
     wgt.resize(600,600);
 
+    //надпись
     QLabel lbl("нажмите левую кнопку мыши в области редактора текста и не отпуская перетащите текст в документ MS Word");
+
+    //текстовый редактор
     TextEdit* edit = new TextEdit("Какой-то текст");
 
+    //вертикальный слой, на который я добавляю виджеты lbl и edit
     QVBoxLayout* layout = new QVBoxLayout;
     layout->addWidget(&lbl);
     layout->addWidget(edit);
 
-
+    //установка слоя на виджет
     wgt.setLayout(layout);
 
     wgt.show();
--- Task1/textedit.h
+++ Task1/textedit.h
@@ -2,35 +2,37 @@
 #define TEXTEDIT_H
 #include <QtWidgets>
 
-class TextEdit : public QTextEdit
+class TextEdit : public QTextEdit //наследую все от QTextEdit
 {
+    //это очень важно, макрос для активации системы сигналов и слотов
     Q_OBJECT
 private:
-    QPoint m_ptDragPos;
+    QPoint m_ptDragPos; //позиция при перемещении
 
     void startDrag(){
-        QMimeData* pMimeData = new QMimeData;
+        QMimeData* pMimeData = new QMimeData; //QMimeData это класс для переноса данных
         selectAll();
         pMimeData->setText(this->toPlainText());
 
+        //инициализируем перемещение
         QDrag* pDrag = new QDrag(this);
         pDrag->setMimeData(pMimeData);
-        pDrag->setPixmap(QPixmap(":/dragImage.png"));
-        pDrag->exec();
+        pDrag->setPixmap(QPixmap(":/dragImage.png")); //с картинкой
+        pDrag->exec();//создаем событие переноса
     }
 protected:
-    virtual void mousePressEvent(QMouseEvent* e){
+    virtual void mousePressEvent(QMouseEvent* e){ //кнопка нажата
         if(e->button() == Qt::LeftButton){
-            m_ptDragPos = e->pos();
+            m_ptDragPos = e->pos(); //запоминаем позицию
         }
-        QTextEdit::mousePressEvent(e);
+        QTextEdit::mousePressEvent(e); //отправляем дальше
     }
 
-    virtual void mouseMoveEvent(QMouseEvent* e){
-        if(e->buttons() & Qt::LeftButton){
+    virtual void mouseMoveEvent(QMouseEvent* e){ //мышка движется
+        if(e->buttons() & Qt::LeftButton){ //зажата LKM и все что угодно еще
             int distance = (e->pos() - m_ptDragPos).manhattanLength();
             if(distance > QApplication::startDragDistance()){
-                startDrag();
+                startDrag(); //перенос
             }
         }
         QTextEdit::mousePressEvent(e);
